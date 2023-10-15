2e4c551d6965c46d977487bd416d58b88c794fe7
ksenia274 <kcen,yackubowa@2013@gmail.com>
2023-10-15 23:03:17 +0300

    Update2 profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,58 +1,86 @@
-// // userProfile.tsx
 'use client'
-import React, {useContext, useEffect} from "react";
+
+
+import React, {useContext} from "react";
 import AuthContext from "@/context/AuthContext";
 import Link from "next/link";
 
-// let authContext: {
-//     signIn: (username: string, password: string) => void;
-//     signOut: (user: Object) => void;
-//     user: { username: string, email: string } | null;
-//     signUp: (user: Object) => void;
-//     authReady: boolean
-// }
 export default function UserProfilePage() {
     let authContext = useContext(AuthContext);
-
+    const [editMode, setEditMode] = React.useState(false);
 
 
     if (!authContext.user) {
-        window.location.href = '/signin';
+        location.href = '/signin';
     }
-    function ClickSignOut(){
-        authContext.user = null
+
+
+    function ClickSignOut() {
+        authContext.signOut(authContext.user!!)
     }
 
 
     return (
         <>
             {(
-                <section className="container mx-auto h-screen flex items-center justify-center">
-                    <div className="w-full max-w-sm">
-                        <h1 className="block text-gray-500 text-center text-xl font-bold mb-4">User Profile</h1>
+                <section className="container mx-auto h-screen flex items-center justify-center width-700">
 
-                        <h2 className="block text-gray-700 text-center text-lg mb-2">
+
+                    <div className="w-full max-w-xxl ">
+
+
+                        <h2 className="block text-white-300 text-center text-xl mb-8">
                             <AuthContext.Consumer>
-                                {(user) => (
-                                    <h3>
-                                        {`Hello, ${user}!`}
-                                    </h3>)
+                                {() => (
+                                    <div>
+
+                                        <div>
+                                            <img className="mx-auto"
+                                                 src={"https://i.pinimg.com/originals/45/73/19/457319eeee8a2028e99293c7b83fa702.jpg"}
+                                                 width="200" height="200"
+                                                 style={{borderRadius: '50%'}}
+                                            />
+
+                                        </div>
+
+
+                                        <h3 className="text-2xl text-orange-400">
+                                            {` ${(authContext.user as { "name": string }).name}`}
+                                        </h3>
+                                        <p className="text-sm text-gray-500">
+                                            Email: {`${(authContext.user as { "email": string }).email}`}
+                                        </p>
+                                        <button type="button" className="text-m text-white-200  text-end"
+                                                onClick={() => setEditMode(!editMode)}>
+                                            {editMode ? 'Save Changes' : 'Edit Profile'}
+                                        </button>
+
+                                        {/* Edit Mode */}
+                                        {editMode && (
+                                            <div>
+                                                {/* Add the editable inputs here */}
+                                            </div>
+                                        )}
+
+                                    </div>)
                                 }
                             </AuthContext.Consumer>
                         </h2>
 
-                        <div className="mx-auto flex items-center justify-center">
 
+                        <div className="mx-auto flex items-end justify-end">
                             <Link href="/signin">
                                 <span
                                     onClick={ClickSignOut}
-                                    className="text-xl text-center text-white block bg-orange-500 p-3 rounded-2xl hover:bg-orange-400">
+                                    className="text-xl text-end text-white block bg-orange-500 p-3 rounded-2xl hover:bg-orange-400">
                                  Sign Out
                                 </span>
                             </Link>
                         </div>
                     </div>
                 </section>
+
+
             )}
         </>
     );
