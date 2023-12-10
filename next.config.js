ff3a1e6f6af08c4bde15c6cd44545df66d93c294
Stephen Ostapenko <stepanos2002@gmail.com>
2023-12-10 17:59:46 +0300

    load api url from .env

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:9999/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ]
   },
